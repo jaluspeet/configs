7cd180ed034602d9c3308864638e96f1eb7020db
jaluspeet <jaluspit@gmail.com>
2023-12-31 16:25:00 +0100

    removed emacs and ssh

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,9 @@
 
 CONFIGS=Projects/configs
 
-mkdir -p $HOME/.emacs.d && ln -sf $HOME/$CONFIGS/emacs/init.el $HOME/.emacs.d
 ln -sf $HOME/$CONFIGS/git/.gitconfig $HOME
 ln -sf $HOME/$CONFIGS/i3 $HOME/.config
 ln -sf $HOME/$CONFIGS/kitty $HOME/.config
 ln -sf $HOME/$CONFIGS/nvim $HOME/.config
 ln -sf $HOME/$CONFIGS/zsh/.zshrc $HOME
 ln -sf $HOME/$CONFIGS/vim/.vimrc $HOME/
-mkdir -p $HOME/.ssh && ln -sf $HOME/$CONFIGS/ssh/config $HOME/.ssh
